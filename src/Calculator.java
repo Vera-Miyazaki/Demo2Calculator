810eb76963492380a3fe40d95b667fa6d5698bbf
vmiyazaki <s50797@edu.campus02.at>
2021-10-08 19:59:56 +0200

    insert variables

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,5 +2,21 @@ public class Calculator {
 
     public static void main(String[] args) {
 
+        int i = 1;
+                double d = 2.3;
+                boolean b = true;
+                char A = 'A';
+                        String Tanga = "Tanga";
+
+        System.out.println(i);
+        System.out.println(d);
+        System.out.println(Tanga);
+        System.out.println(b);
+        System.out.println(A);
+
+
+        Calculator einsZweiDrei = new Calculator();
+
+        System.out.println("einsZweiDrei = " + einsZweiDrei);
     }
 }
